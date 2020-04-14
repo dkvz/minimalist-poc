dff672cf2ad9bda564bf95df78191fdcdf9f7935
DkVZ <bouc@clanz.be>
2020-04-14 21:21:52 +0200

    I need to fix the overflow for the menu in articles.html

--- src/article.js
+++ src/article.js
@@ -2,6 +2,7 @@ const menuBtn = document.getElementById('menuBtn');
 const menuDynamic = document.querySelector('.menu-dynamic'); 
 
 menuBtn.addEventListener('click', () => {
+  document.body.setAttribute('data-overlay', 'true');
   menuDynamic.classList.remove('d-none');
   menuDynamic.classList.add('show');
 });
