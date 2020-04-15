9dac5c359bb41594b3bc1e6d643d781d170b8846
DkVZ <bouc@clanz.be>
2020-04-15 17:10:14 +0200

    Menu shows and hides correctly on article page

--- src/article.js
+++ src/article.js
@@ -1,8 +1,19 @@
 const menuBtn = document.getElementById('menuBtn');
+const closeMenuBtn = document.getElementById('closeMenuBtn');
 const menuDynamic = document.querySelector('.menu-dynamic'); 
 
 menuBtn.addEventListener('click', () => {
   document.body.setAttribute('data-overlay', 'true');
   menuDynamic.classList.remove('d-none');
-  menuDynamic.classList.add('show');
+  setTimeout(() => {
+    menuDynamic.classList.add('show');
+  }, 10);
+});
+
+closeMenuBtn.addEventListener('click', () => {
+  menuDynamic.classList.remove('show');
+  setTimeout(() => {
+    document.body.removeAttribute('data-overlay');
+    menuDynamic.classList.add('d-none');
+  }, 1000);
 });
